831fe4e196d9c026011ad1647ad0eb8cd00d864e
My5t-404 <reeddoy@gmail.com>
2025-01-13 09:55:13 +0600

    first

--- home/models.py
+++ home/models.py
@@ -20,3 +20,74 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+
+class Create_Expense_Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+    create_by = models.CharField(max_length=100, blank=True, null=True)
+    create_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Tour_Details(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    tour_date = models.DateField(blank=True,null=True, auto_now=False)
+    tour_code = models.CharField(max_length=50,blank=True,null=True)
+    total_cost = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Loan_Given_Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    total_paid = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    total_due = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    loan_user_code = models.CharField(max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Expense_List(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    tour_expense = models.BooleanField(default=False)
+    loan_expense = models.BooleanField(default=False)
+    tour_code = models.CharField(max_length=50,blank=True,null=True)
+    loan_code = models.CharField(max_length=50,blank=True,null=True)
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    note = models.TextField(blank=True, null=True)
+    expense_by = models.CharField(max_length=100, blank=True, null=True)   #cash or bank
+    created_by = models.CharField(max_length=100, blank=True, null=True)
+    create_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Income_List(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    loan_income = models.BooleanField(default=False)
+    loan_code = models.CharField(max_length=50,blank=True,null=True)
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True,null=True)
+    note = models.TextField(blank=True, null=True)
+    income_get_by = models.CharField(max_length=100, blank=True, null=True)   #cash or bank
+    created_by = models.CharField(max_length=100, blank=True, null=True)
+    create_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
